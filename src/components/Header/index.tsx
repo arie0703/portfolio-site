8b4edfac8426a6a0feb4c1fa4350e147a0c2f326
K.A <kaiseiarimura0703@gmail.com>
2024-02-03 07:56:25 +0900

    フォント設定の変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,17 @@
 import styles from '@/styles/components/header.module.scss';
+import { Vibur } from 'next/font/google';
+
+const vibur = Vibur({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+})
 
 export const Header = () => {
   return (
     <div className={styles['header']}>
-      <div className={styles['header__logo']}>
-        <b>K. A. portfolio</b>
+      <div className={`${styles['header__logo']} ${vibur.className}`}>
+        <p>K. A. portfolio</p>
       </div>
     </div>
   );
