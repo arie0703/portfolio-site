af2e67569d287c47f4ea5db381b4f985cc753b71
K.A <kaiseiarimura0703@gmail.com>
2024-03-07 08:11:59 +0900

    :construction: WIP: Workコンポーネントの作成

--- src/components/ContentArea/index.tsx
+++ src/components/ContentArea/index.tsx
@@ -1,26 +1,26 @@
-
-'use client'
-import styles from '@/styles/components/content-area.module.scss';
-import { MenuBar } from '../MenuBar';
-import { WorkContainer } from '../WorkContainer';
-import { About } from '../About';
-import { Skills } from '../Skills';
-import { Title } from '@/components/Title';
-import { Contact } from '../Contact';
+"use client";
+import styles from "@/styles/components/content-area.module.scss";
+import { MenuBar } from "../MenuBar";
+import { Work } from "../Work";
+import { About } from "../About";
+import { Skills } from "../Skills";
+import { Title } from "@/components/Title";
+import { Contact } from "../Contact";
 
 export const ContentArea = () => {
-
-  const items = ['About', 'Skills', 'Contact']
+  const items = ["About", "Skills", "Work", "Contact"];
 
   return (
     <div className={styles.switchContent}>
       <MenuBar items={items} />
-      <Title title={'About'} />
+      <Title title={"About"} />
       <About />
-      <Title title={'Skills'} />
+      <Title title={"Skills"} />
       <Skills />
-      <Title title={'Contact'} />
+      <Title title={"Work"} />
+      <Work />
+      <Title title={"Contact"} />
       <Contact />
     </div>
-  )
-}
+  );
+};
--- src/consts/work.ts
+++ src/consts/work.ts
@@ -1,7 +1,12 @@
 export const work = {
-  title: 'みんなのポルトガル語単語帳',
-  imagePath: '/images/portan-app-thumbnail.png',
-  description: 'ポルトガル語の単語帳アプリです。ポルトガル語の単語を登録して、覚えたい単語を管理できます。',
-  workUrl: 'http://localhost:3000',
-  repositoryUrl: 'http://localhost:3000'
-}
+  title: "みんなのポルトガル語単語帳",
+  imagePath: "/images/work/portan-app-thumbnail.png",
+  description:
+    "ポルトガル語の単語帳アプリです。ポルトガル語の単語を登録して、覚えたい単語を管理できます。",
+  repositoryUrl: "https://github.com/arie0703/Portan_app",
+  previewImages: [
+    "/images/work/portan-preview-1.png",
+    "/images/work/portan-preview-2.png",
+    "/images/work/portan-preview-3.png",
+  ],
+};
